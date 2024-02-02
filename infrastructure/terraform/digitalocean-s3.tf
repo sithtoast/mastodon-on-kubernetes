5dda54e2c968ea0168bf657f8e02692c08802946
William Holt <william@Williams-Mac-Studio.local>
2024-02-02 10:10:34 -0500

    Digitalocean bootstrap

--- infrastructure/terraform/digitalocean-s3.tf
+++ infrastructure/terraform/digitalocean-s3.tf
@@ -2,5 +2,4 @@ resource "digitalocean_spaces_bucket" "mastodon_spaces" {
   count  = var.enable_external_s3 ? 1 : 0
   name   = var.s3_bucket_name
   region = var.s3_bucket_region
-  acl    = "private"
 }
